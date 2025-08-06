e630a0bb7f55b7bd5c32c09bfffdb654b7d07cb6
TONE Lawdoin Aimé <enotal12@gmail.com>
2025-08-06 16:14:59 +0000

    front with keycloakService

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -8,29 +8,13 @@ import myBackground from '../assets/images/background.jpg'
 const DefaultLayout = () => {
   const isLoggedIn = isAuthenticated()
   return (
-<<<<<<< HEAD
     <div className="defaultLayout-main-container">
-=======
-    <div
-      className="defaultLayout-main-container"
-      // style={{
-      //   backgroundImage: `url(${isLoggedIn ? '' : myBackground})`,
-      //   backgroundSize: 'cover',
-      //   backgroundPosition: 'center',
-      //   backgroundRepeat: 'no-repeat',
-      // }}
-    >
->>>>>>> ac3612064d474c848834f5ba3844b849ca83f5a7
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
         <div className="body flex-grow-1">
           <AppContent />
         </div>
-<<<<<<< HEAD
-=======
-        <AppFooter />
->>>>>>> ac3612064d474c848834f5ba3844b849ca83f5a7
       </div>
     </div>
   )
