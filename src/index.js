3c7376420e3c21a1f583579c287a0074fcff14ef
enotal <enotal12@gmail.com>
2026-01-23 11:18:50 +0000

    commit23012026

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import 'core-js'
 
 import App from './App'
 import store from './store'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </Provider>,
 )
